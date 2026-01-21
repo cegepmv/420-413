3744ca943894e411c301806ab6747e2793825dcf
Samuel Fostiné <samuel.fostine@cegepmv.ca>
2026-01-21 04:59:02 -0500

    first commit to create site

---
+++ content/_index.md
@@ -0,0 +1,40 @@
++++
+title = "Développement d'applications pour entreprises"
+type = "home"
++++
+
+
+***
+
+## Plans de cours
+
+:clipboard: Samuel Fostiné  : <a href="/420-413/Plan_de_cours_420_413_MV_SAMUEL.pdf" target="_blanck">Plan de cours</a>
+
+
+***
+
+## Horaire et disponibilités des enseignants
+
+:heavy_check_mark: Samuel Fostiné  : <a href="/420-413/horaire_hiver2026.pdf" target="_blanck">Lien vers mon horaire et mes disponibilités</a>
+
+***
+
+##  Samuel Fostiné 
+
+|Nom de l'évaluation|Pondération|Semaine de l'évaluation|
+|:---:|:---:|:---:|
+|10 quiz (8 meilleures notes)| 15 % |Semaine 2 à 14|
+|Examen 1| 20% |Semaine 4|
+|Examen 2| 26 % |Semaine 12|
+|Projet remise 1| 5 % |Semaine 6|
+|Projet remise 2| 5 % |Semaine 8|
+|Projet remise 3| 5 % |Semaine 10|
+|Projet remise 4| 5 % |Semaine 13|
+|Projet remise final| 20 % |Semaine 15|
+
+
+
+
+
+
+
---
+++ public/searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Développement d'applications pour entreprises",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/420-413/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Plans de cours 📋 Samuel Fostiné : Plan de cours\nHoraire et disponibilités des enseignants ✔️ Samuel Fostiné : Lien vers mon horaire et mes disponibilités\nSamuel Fostiné Nom de l’évaluation Pondération Semaine de l’évaluation 10 quiz (8 meilleures notes) 15 % Semaine 2 à 14 Examen 1 20% Semaine 4 Examen 2 26 % Semaine 12 Projet remise 1 5 % Semaine 6 Projet remise 2 5 % Semaine 8 Projet remise 3 5 % Semaine 10 Projet remise 4 5 % Semaine 13 Projet remise final 20 % Semaine 15",
+    "description": "Plans de cours 📋 Samuel Fostiné : Plan de cours\nHoraire et disponibilités des enseignants ✔️ Samuel Fostiné : Lien vers mon horaire et mes disponibilités\nSamuel Fostiné Nom de l’évaluation Pondération Semaine de l’évaluation 10 quiz (8 meilleures notes) 15 % Semaine 2 à 14 Examen 1 20% Semaine 4 Examen 2 26 % Semaine 12 Projet remise 1 5 % Semaine 6 Projet remise 2 5 % Semaine 8 Projet remise 3 5 % Semaine 10 Projet remise 4 5 % Semaine 13 Projet remise final 20 % Semaine 15",
+    "tags": [],
+    "title": "Développement d'applications pour entreprises",
+    "uri": "/420-413/index.html"
+  },
+  {
+    "breadcrumb": "Développement d'applications pour entreprises",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/420-413/tags/index.html"
+  }
+]
