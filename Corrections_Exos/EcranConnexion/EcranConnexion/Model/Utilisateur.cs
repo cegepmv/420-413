143e7823814489addbf19e3b38a189c1ebf9f6b3
Samuel Fostiné <samuel.fostine@cegepmv.ca>
2026-03-06 10:03:54 -0500

    ajout de l'exo sur MVVM

---
+++ Corrections_Exos/EcranConnexion/EcranConnexion/Model/Utilisateur.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcranConnexion.Model
+{
+    public class Utilisateur
+    {
+        public int Id { get; set; }
+        public string Nom { get; set;}
+        public string Prenom { get; set; }
+        public string MotDePasse { get; set; }
+
+    }
+}
---
+++ Corrections_Exos/EcranConnexion/EcranConnexion/ViewModel/ViewModelUtilisateur.cs
@@ -0,0 +1,33 @@
+﻿using EcranConnexion.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcranConnexion.ViewModel
+{
+    internal class ViewModelUtilisateur : INotifyPropertyChanged
+    {
+        Utilisateur utilisateur;
+
+        public ViewModelUtilisateur()
+        {
+            utilisateur = new Utilisateur();
+        }
+
+        public string Nom
+        {
+            get => utilisateur.Nom;
+            set
+            {
+                utilisateur.Nom = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Nom"));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+    }
+}
